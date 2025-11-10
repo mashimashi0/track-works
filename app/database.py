ef2f99f1cd8aba56005d73dbc4b7c5fbbaf39e3b
mashimashi0 <masajob48@gmail.com>
2025-11-10 14:57:13 +0900

    更新

--- app/database.py
+++ app/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-# ✅ MySQL に変更
-# 例: root / パスワードなし / ローカル の場合
-DATABASE_URL = "mysql+pymysql://root:hogehoge1234@127.0.0.1:3306/recipes_db"
+# PostgreSQL 用
+load_dotenv()  # .env があれば読み込む
+
+# 環境変数から読み込み、未定義ならデフォルトを使用
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:password@db:5432/recipes_db"
+)
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ sql/create.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE IF NOT EXISTS recipes (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  making_time VARCHAR(100) NOT NULL,
+  serves VARCHAR(100) NOT NULL,
+  ingredients VARCHAR(300) NOT NULL,
+  cost INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO recipes (
+  id,
+  title,
+  making_time,
+  serves,
+  ingredients,
+  cost,
+  created_at,
+  updated_at
+)
+VALUES (
+  1,
+  'チキンカレー',
+  '45分',
+  '4人',
+  '玉ねぎ,肉,スパイス',
+  1000,
+  '2016-01-10 12:10:12',
+  '2016-01-10 12:10:12'
+);
+
+INSERT INTO recipes (
+  id,
+  title,
+  making_time,
+  serves,
+  ingredients,
+  cost,
+  created_at,
+  updated_at
+)
+VALUES (
+  2,
+  'オムライス',
+  '30分',
+  '2人',
+  '玉ねぎ,卵,スパイス,醤油',
+  700,
+  '2016-01-11 13:10:12',
+  '2016-01-11 13:10:12'
+);
